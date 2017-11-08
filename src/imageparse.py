3f65d644b97c19f1a1b6dc8f194ec61b6f482458
OrangeJewc <samnayerman@gmail.com>
2017-11-07 16:10:34 -0800

    poop

--- src/imageparse.py
+++ src/imageparse.py
@@ -1,6 +1,7 @@
 from PIL import Image, ImageFile, ImageStat, ImageFilter, ImageChops
 import io
 import numpy
+import math
 
 def trim(im):
     bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
@@ -11,7 +12,9 @@ def trim(im):
         return im.crop(bbox)
 
 def main():
-   imagePath = "bird.png"
+   imagePath = "cactusman.jpg"
+   # imagePath = "splice.png"
+   # imagePath = "bird.png"
    ImageFile.LOAD_TRUNCATED_IMAGES = True
 
    image = Image.open(imagePath).convert("RGB")
@@ -19,26 +22,64 @@ def main():
 
    trimmedImage = trim(image)
 
-   #Image Info (w/o background color)
-   width = trimmedImage.width
-   height = trimmedImage.height
-
-   # [(count, (R, G, B)), ...]
-   colors = image.getcolors()
-   pixels = list(image.getdata())
-
-   #Color Info (from each band)
-   averageColor = ImageStat.Stat(image).mean
-   minColor = tuple(pixel[0] for pixel in ImageStat.Stat(image).extrema)
-   maxColor = tuple(pixel[1] for pixel in ImageStat.Stat(image).extrema)
-   medianColor = ImageStat.Stat(image).median
-   stddev = ImageStat.Stat(image).stddev
-
    # image.save("parsed.png", format="PNG")
-   # out = image.filter(ImageFilter.FIND_EDGES)
+   # out = trimmedImage.filter(ImageFilter.FIND_EDGES)
+   # out.show()
+   fitnessFunction(trimmedImage)
    trimmedImage.save("out.png", format="PNG")
 
+def fitnessFunction(image):
+  #Image Info (w/o background color)
+  width = image.width
+  height = image.height
 
+  # [(count, (R, G, B)), ...]
+  colors = image.getcolors()
+  pixels = list(image.getdata())
+
+  #Color Info (from each band)
+  averageColor = ImageStat.Stat(image).mean
+  minColor = tuple(pixel[0] for pixel in ImageStat.Stat(image).extrema)
+  maxColor = tuple(pixel[1] for pixel in ImageStat.Stat(image).extrema)
+  medianColor = ImageStat.Stat(image).median
+  stddev = ImageStat.Stat(image).stddev
+
+  #squares the value of each band, adds them together, and takes the sqrt of the value
+  medianVal = math.sqrt((medianColor[0]**2) + (medianColor[1]**2) + (medianColor[2]**2))
+  averageVal = math.sqrt((averageColor[0]**2) + (averageColor[1]**2) + (averageColor[2]**2))
+  stddevVal = math.sqrt((stddev[0]**2) + (stddev[1]**2) + (stddev[2]**2))
+
+  fitnessScore = 0
+
+  # chi square analysis
+  # if we want/like more variety in colors (lower colorVarianceScore) then
+  # the farther away the average is from median, the worse the fitness score since median is always middle of the results
+  # less color variety = higher colorVarianceScore
+  colorVarianceScore = (1/((medianVal - averageVal)**2/averageVal))*10
+  # print colorVarianceScore
+
+  # if width > height -> AR > 1
+  # if height > width -> AR < 1
+  # if width = height -> AR = 1
+  aspectRatio = (float(width)/float(height))
+
+  # higher sizeScore = larger image
+  # lowewr sizeScore = smaller image
+  imageArea = float(width) * float(height)
+  sizeScore = math.sqrt(imageArea)*0.1
+  # print sizeScore
+
+  # if we want large images, add sizescore
+  # if we want small images, subtract sizeScore
+  # if we dont care about size, ignore sizeScore
+
+  # if we want more colors, low colorVarianceScore is good
+  # if we want less colors, high colorVarianceScore is good
+
+  # large image, high color variance
+  fitnessScore = colorVarianceScore + sizeScore
+
+  # print width, height, aspectRatio, sizeScore
 
 if __name__ == '__main__':
     main()
